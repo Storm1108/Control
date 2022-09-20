2016b8fe35c526b14bb10e31fc4064bf9d7a86f5
Storm1108 <mail.laser@yandex.ru>
2022-09-21 01:16:13 +0300

    Финальная версия

---
+++ Programm/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] stringArray = {"sgvsg", "SVGS", "MJ1", "12", "qwerty", "as", "k00", "-100"};
+string[] ResultArray(string[] array){
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            j++;
+        }
+    }
+    string[] sortedArray = new string[j];
+    j=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            sortedArray[j] = array[i];
+            j++;
+        }
+    }
+    return sortedArray;
+}
+void PrintArray(string[] array){
+    if (array.Length != 0)
+    {
+    System.Console.WriteLine($"({string.Join(", ", array)})");    
+    }
+    else
+    {
+        System.Console.WriteLine("Результирующий массив пуст");
+    }
+    
+}
+PrintArray(stringArray);
+PrintArray(ResultArray(stringArray));
